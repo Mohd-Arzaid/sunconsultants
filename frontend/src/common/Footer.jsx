07fcc9958bd4df2c510ed8650e2a49de9f4b0101
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-03-21 12:08:29 +0530

    working on footer

--- frontend/src/common/Footer.jsx
+++ frontend/src/common/Footer.jsx
@@ -13,13 +13,16 @@ const Footer = () => {
         {/* Bottom Bar */}
         <div className="border border-[#7eafd9]"></div>
 
-        <a
-          href="https://github.com/Mohd-Arzaid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <GitHubLogoIcon className=" h-6 lg:h-6 w-8 lg:w-8 duration-200 hover:-translate-y-2 cursor-pointer" />
-        </a>
+        <div className="flex gap-3 flex-wrap mt-4 md:mt-3 mb-4  md:mb-3 items-center">
+          <a
+            href="https://github.com/Mohd-Arzaid"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GitHubLogoIcon className=" h-6 lg:h-6 w-8 lg:w-8 duration-200 hover:-translate-y-2 cursor-pointer" />
+          </a>
+        </div>
+
 
 
 
