b94a4c7b03bd6da79a0d7999c600eb7aebe8e377
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-05-19 17:51:31 +0530

    change icon

--- frontend/src/components/manual/BISCertification.jsx
+++ frontend/src/components/manual/BISCertification.jsx
@@ -177,20 +177,10 @@ const BISCIndex = () => {
             viewBox="0 0 24 24"
             stroke="currentColor"
           >
-            {isMobileMenuOpen ? (
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M6 18L18 6M6 6l12 12"
-              />
+    {isMobileMenuOpen ? (
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
             ) : (
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
             )}
           </svg>
         </button>
