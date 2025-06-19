0c20e89896cc3241d01e3b01af5e6e6689e6d6cc
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-06-19 14:15:24 +0530

    fixing

--- frontend/src/pages/InternationalAudits.jsx
+++ frontend/src/pages/InternationalAudits.jsx
@@ -1,16 +1,16 @@
 import Footer from "@/common/Footer";
 import { Services } from "./Home";
 
-import BahrainAudit from "../assets/auditImages/Bahrain.png"
-import IndonesiaAudit from "../assets/auditImages/Indonesia.png"
-import ItalyAudit from "../assets/auditImages/Italy-2.png"
-import MalaysiaAudit from "../assets/auditImages/Malaysia.png"
-import SloveniaAudit from "../assets/auditImages/Slovenia.png"
-import ThailandAudit from "../assets/auditImages/Thailand.png"
-import Vietnam2Audit from "../assets/auditImages/Vietnam-2.png"
-import Vietnam3Audit from "../assets/auditImages/Vietnam-3.png"
-import Vietnam4Audit from "../assets/auditImages/Vietnam-4.png"
-import VietnamAudit from "../assets/auditImages/Vietnam.png"
+import BahrainAudit from "../assets/auditImages/Bahrain.png";
+import IndonesiaAudit from "../assets/auditImages/Indonesia.png";
+import ItalyAudit from "../assets/auditImages/Italy-2.png";
+import MalaysiaAudit from "../assets/auditImages/Malaysia.png";
+import SloveniaAudit from "../assets/auditImages/Slovenia.png";
+import ThailandAudit from "../assets/auditImages/Thailand.png";
+import Vietnam2Audit from "../assets/auditImages/Vietnam-2.png";
+import Vietnam3Audit from "../assets/auditImages/Vietnam-3.png";
+import Vietnam4Audit from "../assets/auditImages/Vietnam-4.png";
+import VietnamAudit from "../assets/auditImages/Vietnam.png";
 
 const InternationalAudits = () => {
   return (
@@ -18,7 +18,9 @@ const InternationalAudits = () => {
       {/* Audit Hero */}
       <AuditHero />
       <AuditGallery />
-      <Services />
+      <div id="services-section">
+        <Services />
+      </div>
       <Footer />
     </div>
   );
@@ -27,6 +29,11 @@ const InternationalAudits = () => {
 export default InternationalAudits;
 
 const AuditHero = () => {
+  const handleScroll = () => {
+    const element = document.getElementById("services-section");
+    element?.scrollIntoView({ behavior: "smooth", block: "start" });
+  };
+
   return (
     <div className="max-w-[88rem] mx-auto px-4 py-8 md:px-12 md:py-12">
       {/* Heading */}
@@ -44,21 +51,28 @@ const AuditHero = () => {
 
       {/* Button */}
       <div className="flex flex-col md:flex-row items-center justify-center gap-4 md:gap-6 mb-10">
-        <button className="w-full md:w-auto flex items-center justify-center gap-3 bg-[#1A8781] text-white py-3 px-8 rounded-full shadow-lg hover:bg-[#125E5A] transition-all duration-300 group">
+        <button
+          onClick={handleScroll}
+          className="w-full md:w-auto flex items-center justify-center gap-3 bg-[#1A8781] text-white py-3 px-8 rounded-full shadow-lg hover:bg-[#125E5A] transition-all duration-300 group"
+        >
           <span className="font-medium font-geist">Our Services</span>
           <div className="w-6 h-6 rounded-full bg-white/20 flex items-center justify-center group-hover:bg-white/30">
             <div className="w-2 h-2 border-t-2 border-r-2 border-white rotate-45"></div>
           </div>
         </button>
 
-        <button className="w-full md:w-auto items-center gap-3 border-2 border-[#1A8781]/30 text-[#1A8781] py-3 px-8 rounded-full hover:border-[#1A8781] hover:bg-[#1A8781]/5 transition-all duration-300">
-          <span className="font-medium font-geist">Contact Us</span>
-        </button>
+        <a href="/contact">
+          <button className="w-full md:w-auto items-center gap-3 border-2 border-[#1A8781]/30 text-[#1A8781] py-3 px-8 rounded-full hover:border-[#1A8781] hover:bg-[#1A8781]/5 transition-all duration-300">
+            <span className="font-medium font-geist">Contact Us</span>
+          </button>
+        </a>
       </div>
 
       <h2 className="text-3xl md:text-4xl text-center mb-4">
         {/* <span className="text-[#0A4394] font-geist font-medium mr-1 ">/</span> */}
-        <span className="font-playfair font-bold text-[#1e1e1e]">Audit Glimpse</span>
+        <span className="font-playfair font-bold text-[#1e1e1e]">
+          Audit Glimpse
+        </span>
       </h2>
 
       <p className="text-base md:text-lg text-center font-geist text-gray-600 max-w-xl md:max-w-xl mx-auto leading-relaxed">
