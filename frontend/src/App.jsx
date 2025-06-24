af7a1ad3d1bb47e08907c109443904267f4be496
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-06-24 17:09:01 +0530

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,54 +1,50 @@
 import { Route, Routes } from "react-router-dom";
-import { lazy, Suspense } from "react";
+import { useState } from "react";
 import Navbar from "./common/Navbar";
 import TopBar from "./common/TopBar";
 import MobileNav from "./components/common/MobileNav";
 import SocialFloatingButtons from "./components/common/SocialFloatingButtons";
 import ContactFormPopup from "./components/ContactFormPopup";
 import ScrollToTopButton, { ScrollToTop } from "./components/common/ScrollToTop";
-import { useState } from "react";
 
-// No loading component - instant navigation
-const PageLoader = () => null;
+// Direct imports for instant navigation - no lazy loading
+import Home from "./pages/Home";
+import About from "./pages/About";
+import ContactUs from "./pages/ContactUs";
+import Notification from "./pages/Notification";
+import NotificationDetail from "./pages/NotificationDetail";
+import MinistryUpdates from "./pages/MinistryUpdates";
+import InternationalAudits from "./pages/InternationalAudits";
+import Exhibition from "./pages/Exhibition";
+import Sitemap from "./pages/Sitemap";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsAndConditions from "./pages/TermsAndConditions";
+import Error404 from "./pages/404Error";
+import Webinar from "./pages/Webinar";
 
-// Lazy load all page components
-const Home = lazy(() => import("./pages/Home"));
-const About = lazy(() => import("./pages/About"));
-const ContactUs = lazy(() => import("./pages/ContactUs"));
-const Notification = lazy(() => import("./pages/Notification"));
-const NotificationDetail = lazy(() => import("./pages/NotificationDetail"));
-const MinistryUpdates = lazy(() => import("./pages/MinistryUpdates"));
-const InternationalAudits = lazy(() => import("./pages/InternationalAudits"));
-const Exhibition = lazy(() => import("./pages/Exhibition"));
-const Sitemap = lazy(() => import("./pages/Sitemap"));
-const PrivacyPolicy = lazy(() => import("./pages/PrivacyPolicy"));
-const TermsAndConditions = lazy(() => import("./pages/TermsAndConditions"));
-const Error404 = lazy(() => import("./pages/404Error"));
-const Webinar = lazy(() => import("./pages/Webinar"));
+// Service pages - direct imports
+import CDSCO from "./pages/CDSCO";
+import SchemeX from "./pages/SchemeX";
+import CRSRegistration from "./pages/CRSRegistration";
+import Peso from "./pages/Peso";
+import Tec from "./pages/Tec";
+import WPC from "./pages/WPC";
+import ROHS from "./pages/ROHS";
+import BEE from "./pages/BEE";
+import CECertification from "./pages/CECertification";
+import EMIEMC from "./pages/EMIEMC";
+import CBCertification from "./pages/CBCertification";
+import ISIMark from "./pages/ISIMark";
+import BatteryWaste from "./pages/BatteryWaste";
 
-// Service pages - lazy loaded
-const CDSCO = lazy(() => import("./pages/CDSCO"));
-const SchemeX = lazy(() => import("./pages/SchemeX"));
-const CRSRegistration = lazy(() => import("./pages/CRSRegistration"));
-const Peso = lazy(() => import("./pages/Peso"));
-const Tec = lazy(() => import("./pages/Tec"));
-const WPC = lazy(() => import("./pages/WPC"));
-const ROHS = lazy(() => import("./pages/ROHS"));
-const BEE = lazy(() => import("./pages/BEE"));
-const CECertification = lazy(() => import("./pages/CECertification"));
-const EMIEMC = lazy(() => import("./pages/EMIEMC"));
-const CBCertification = lazy(() => import("./pages/CBCertification"));
-const ISIMark = lazy(() => import("./pages/ISIMark"));
-const BatteryWaste = lazy(() => import("./pages/BatteryWaste"));
-
-// Manual components - lazy loaded
-const AllClients = lazy(() => import("./components/manual/AllClients"));
-const BISFM = lazy(() => import("./components/manual/BISFM").then(module => ({ default: module.BISFM })));
-const EPRService = lazy(() => import("./components/manual/EPRService").then(module => ({ default: module.EPRService })));
-const BISCertification = lazy(() => import("./components/manual/BISCertification").then(module => ({ default: module.BISCertification })));
-const LMPC = lazy(() => import("./components/manual/LMPC").then(module => ({ default: module.LMPC })));
-const PlasticWaste = lazy(() => import("./components/manual/PlasticWaste").then(module => ({ default: module.PlasticWaste })));
-const LegalMetrology = lazy(() => import("./components/manual/LegalMetrology").then(module => ({ default: module.LegalMetrology })));
+// Manual components - direct imports with named exports
+import AllClients from "./components/manual/AllClients";
+import { BISFM } from "./components/manual/BISFM";
+import { EPRService } from "./components/manual/EPRService";
+import { BISCertification } from "./components/manual/BISCertification";
+import { LMPC } from "./components/manual/LMPC";
+import { PlasticWaste } from "./components/manual/PlasticWaste";
+import { LegalMetrology } from "./components/manual/LegalMetrology";
 
 function App() {
   const [popupOpen, setPopupOpen] = useState(false);
@@ -60,88 +56,86 @@ function App() {
       <ScrollToTopButton hide={popupOpen} />
       <ScrollToTop />
       
-      <Suspense fallback={<PageLoader />}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/bis-qco-updates" element={<Notification />} />
-          <Route path="/bis-qco-updates/:notificationName" element={<NotificationDetail />} />
-          <Route path="/ministry-updates" element={<MinistryUpdates />} />
-          <Route path="/international-audits" element={<InternationalAudits />} />
-          <Route path="/seminars-and-exhibitions" element={<Exhibition />} />
-          <Route path="/clients" element={<AllClients />} />
-          <Route path="/sitemap" element={<Sitemap />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/bis-qco-updates" element={<Notification />} />
+        <Route path="/bis-qco-updates/:notificationName" element={<NotificationDetail />} />
+        <Route path="/ministry-updates" element={<MinistryUpdates />} />
+        <Route path="/international-audits" element={<InternationalAudits />} />
+        <Route path="/seminars-and-exhibitions" element={<Exhibition />} />
+        <Route path="/clients" element={<AllClients />} />
+        <Route path="/sitemap" element={<Sitemap />} />
 
-          {/* Services Pages */}
-          <Route path="/cdsco-registration-certification" element={<CDSCO />} />
-          <Route path="/schemeX" element={<SchemeX />} />
-          <Route
-            path="/a-guide-to-bis-certification-for-foreign-manufacturers-indian-bis"
-            element={<BISFM />}
-          />
-          <Route
-            path="/a-guide-on-how-to-obtain-epr-certificate"
-            element={<EPRService />}
-          />
-          <Route
-            path="/what-is-bis-certificate-indian-bis"
-            element={<BISCertification />}
-          />
-          <Route
-            path="/a-guide-on-how-to-obtain-lmpc-certificate"
-            element={<LMPC />}
-          />
-          <Route
-            path="/epr-certificate-for-plastic-waste-management-pwm"
-            element={<PlasticWaste />}
-          />
-          <Route
-            path="/what-is-legal-metrology-or-lmpc-certificate"
-            element={<LegalMetrology />}
-          />
-          <Route
-            path="a-guide-to-bis-certification-indian-bis"
-            element={<ISIMark />}
-          />
-          <Route
-            path="/epr-certificate-for-battery-waste-management-bwm"
-            element={<BatteryWaste />}
-          />
-          <Route
-            path="/what-is-crs-bis-or-crs-registration"
-            element={<CRSRegistration />}
-          />
-          <Route
-            path="/information-about-peso-certification-peso-license-india"
-            element={<Peso />}
-          />
-          <Route
-            path="/information-about-tec-certificate-mtcte"
-            element={<Tec />}
-          />
-          <Route
-            path="/information-about-wpc-certificate-eta-approval"
-            element={<WPC />}
-          />
-          <Route
-            path="/restriction-of-hazardous-substance-rohs-certificate"
-            element={<ROHS />}
-          />
-          <Route path="/bee-certification" element={<BEE />} />
-          <Route path="/ce-certification" element={<CECertification />} />
-          <Route path="/emi-emc-certification" element={<EMIEMC />} />
-          <Route path="/cb-certification" element={<CBCertification />} />
-          {/* Service Page End */}
+        {/* Services Pages */}
+        <Route path="/cdsco-registration-certification" element={<CDSCO />} />
+        <Route path="/schemeX" element={<SchemeX />} />
+        <Route
+          path="/a-guide-to-bis-certification-for-foreign-manufacturers-indian-bis"
+          element={<BISFM />}
+        />
+        <Route
+          path="/a-guide-on-how-to-obtain-epr-certificate"
+          element={<EPRService />}
+        />
+        <Route
+          path="/what-is-bis-certificate-indian-bis"
+          element={<BISCertification />}
+        />
+        <Route
+          path="/a-guide-on-how-to-obtain-lmpc-certificate"
+          element={<LMPC />}
+        />
+        <Route
+          path="/epr-certificate-for-plastic-waste-management-pwm"
+          element={<PlasticWaste />}
+        />
+        <Route
+          path="/what-is-legal-metrology-or-lmpc-certificate"
+          element={<LegalMetrology />}
+        />
+        <Route
+          path="a-guide-to-bis-certification-indian-bis"
+          element={<ISIMark />}
+        />
+        <Route
+          path="/epr-certificate-for-battery-waste-management-bwm"
+          element={<BatteryWaste />}
+        />
+        <Route
+          path="/what-is-crs-bis-or-crs-registration"
+          element={<CRSRegistration />}
+        />
+        <Route
+          path="/information-about-peso-certification-peso-license-india"
+          element={<Peso />}
+        />
+        <Route
+          path="/information-about-tec-certificate-mtcte"
+          element={<Tec />}
+        />
+        <Route
+          path="/information-about-wpc-certificate-eta-approval"
+          element={<WPC />}
+        />
+        <Route
+          path="/restriction-of-hazardous-substance-rohs-certificate"
+          element={<ROHS />}
+        />
+        <Route path="/bee-certification" element={<BEE />} />
+        <Route path="/ce-certification" element={<CECertification />} />
+        <Route path="/emi-emc-certification" element={<EMIEMC />} />
+        <Route path="/cb-certification" element={<CBCertification />} />
+        {/* Service Page End */}
 
-          <Route path="/webinar" element={<Webinar />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+        <Route path="/webinar" element={<Webinar />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
 
-          {/* Catch-all route - shows 404 error page for unmatched URLs */}
-          <Route path="*" element={<Error404 />} />
-        </Routes>
-      </Suspense>
+        {/* Catch-all route - shows 404 error page for unmatched URLs */}
+        <Route path="*" element={<Error404 />} />
+      </Routes>
       
       <MobileNav />
       <SocialFloatingButtons hide={popupOpen} />
