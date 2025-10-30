148b04d2c64fa1d798edaa9ef3bfa1b5d0d44a1f
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-10-30 14:58:28 +0530

    fk it

--- frontend/src/data/logo-ticker-data/logo-ticker-data.js
+++ frontend/src/data/logo-ticker-data/logo-ticker-data.js
@@ -5,42 +5,47 @@ const ansellLogo = "/client-logos/ansell.svg";
 const ametekLogo = "/client-logos/ametek.svg";
 const boschLogo = "/client-logos/bosch.svg";
 
-
 export const COMPANY_LOGOS = [
   {
     name: "Honeywell",
     src: honeywellLogo,
     height: 37,
+    width: 160,
     alt: "Honeywell Logo",
   },
   {
     name: "IKEA",
     src: ikeaLogo,
     height: 30,
+    width: 160,
     alt: "IKEA Logo",
   },
   {
     name: "Panasonic",
     src: panasonicLogo,
     height: 26,
+    width: 160,
     alt: "Panasonic Logo",
   },
   {
     name: "Ansell",
     src: ansellLogo,
     height: 26,
+    width: 160,
     alt: "Ansell Logo",
   },
   {
     name: "Ametek",
     src: ametekLogo,
     height: 23.5,
+    width: 160,
     alt: "Ametek Logo",
   },
   {
     name: "Bosch",
     src: boschLogo,
     height: 32,
+    width: 160,
     alt: "Bosch Logo",
   },
 ];
