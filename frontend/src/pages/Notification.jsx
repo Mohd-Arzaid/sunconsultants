6132db76daae0a7419679b626218afc93a51d8f2
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-11-17 15:01:20 +0530

    fk it

--- frontend/src/pages/Notification.jsx
+++ frontend/src/pages/Notification.jsx
@@ -171,7 +171,7 @@ const NotificationMainContent = () => {
         {/* Heading */}
         <div className="text-center mb-6 md:mb-10">
           <h1 className="font-playfair text-2xl md:text-5xl font-bold text-[#1e1e1e] mb-3 md:mb-4">
-            DRAFT QCO Notifications
+            QCO Notifications
           </h1>
           <p className="text-base md:text-lg font-geist text-gray-600 max-w-2xl mx-auto">
             Stay updated with the latest Quality Control Order notifications and
