91520f49c1916b3467ba71becdc76209f49ebffd
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-12-19 10:29:22 +0530

    fk it

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import "non.geist";
 // import { Toaster } from 'sonner'
 import { Toaster } from "@/components/ui/toaster";
 import { HelmetProvider } from "react-helmet-async";
