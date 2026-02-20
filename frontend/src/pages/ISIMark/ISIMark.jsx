6ef9c565f91ec779688d966919f90fbc88d0c02d
Sun Certifications <admin@bis-certifications.com>
2026-02-20 16:06:46 +0530

    fk it

--- frontend/src/pages/ISIMark/ISIMark.jsx
+++ frontend/src/pages/ISIMark/ISIMark.jsx
@@ -818,7 +818,7 @@ const OverviewSection = () => {
         <Separator className="service-left-content-separator" />
       </div>
       <h1 className="service-left-content-heading-two">
-        BIS Certification Schemes in India
+      ISI Mark Certification Scheme
       </h1>
       <div className="flex justify-center mt-[20px] md:mt-[26px]">
         <img
